0678b1911e4d88f8d83f57dce7bca303662d653b
Aaron Reilly <x17124719@student.ncirl.ie>
2019-04-13 10:03:09 +0100

    Added new welcome GUI

--- GameApp/GameApp/src/gameapp/GameApp.java
+++ GameApp/GameApp/src/gameapp/GameApp.java
@@ -16,7 +16,7 @@ public class GameApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        GameGUI gameApp = new GameGUI();
+        WelcomeGUI gameApp = new WelcomeGUI();
         gameApp.setVisible(true);
     }
     
